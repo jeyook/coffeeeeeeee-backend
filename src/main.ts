7772b0405d09d8478f6f4f2553e676c3775897cd
joonho <minhora1@gmail.com>
2024-01-08 01:41:13 +0900

    feat: SWAGGER 적용

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -11,6 +11,14 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('COFFEEEEEEE')
+    .setDescription('COFFEEEEEEE API 명세서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -4,14 +4,48 @@ import { CommonResponseDto } from 'src/common/common-response.dto';
 import { ResponseMessage } from 'src/common/response-message.enum';
 import { Cafe } from '../entity/cafe.entity';
 import { PlaceExceptionFilter } from 'src/filter/place-exception.filter';
+import { ApiCreatedResponse, ApiForbiddenResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('place')
+@ApiTags('place')
 //일단 필터 생성
 @UseFilters(new PlaceExceptionFilter())
 export class PlaceController {
   constructor(private readonly placeService: PlaceService) {}
 
   @Get('/:placeId')
+  @ApiOperation({ summary: 'getPlaceById', description: 'placeId로 해당 카페 상세내용을 가져온다' })
+  @ApiCreatedResponse({
+    description: '성공여부',
+    schema: {
+      example: {
+        statusCode: 200,
+        message: 'READ_SUCCESS',
+        data: {
+          createdAt: '2024-01-07T05:25:58.385Z',
+          updatedAt: '2024-01-07T05:25:58.385Z',
+          deletedAt: null,
+          id: 1,
+          placeId: 1,
+          address: '강남구',
+          name: '스타벅스',
+          mapX: 11,
+          mapY: 12,
+          phoneNumber: '010000000',
+          imageUrl: 'qewr',
+          homepageUrl: 'http://www.starbucks.co.kr',
+        },
+      },
+    },
+  })
+  @ApiForbiddenResponse({
+    schema: {
+      example: {
+        statusCode: 400,
+        message: 'Validation failed (numeric string is expected)',
+      },
+    },
+  })
   async getPlaceById(
     @Param('placeId', ParseIntPipe) placeId: number, //transform true
   ): Promise<CommonResponseDto<Cafe>> {
