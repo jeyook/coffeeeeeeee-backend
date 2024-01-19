e94e70bf39a7781e138c2275379a632685f0ff8d
JunwooPark <wnsdn1994@gmail.com>
2024-01-19 21:47:17 +0900

    [COF-048] fix: bookmark controller return값 명시

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -6,20 +6,28 @@ import { AuthUserData } from '../auth/decorator/auth-user-data.decorator';
 import { CommonResponseDto } from '../common/dto/common-response.dto';
 import { ResponseMessage } from '../common/dto/response-message.enum';
 import { PageRequestDto } from '../common/dto/page-request.dto';
+import { PageResponseDto } from 'src/common/dto/page-response.dts';
+import { BookmarkResponseDto } from './dto/bookmark-resposnse.dto';
 
 @Controller('bookmark')
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
   @Get()
   @UseGuards(TokenAuthGuard)
-  async getPaginatedBookmark(@AuthUserData() user: User, @Query() dto: PageRequestDto) {
+  async getPaginatedBookmark(
+    @AuthUserData() user: User,
+    @Query() dto: PageRequestDto,
+  ): Promise<CommonResponseDto<PageResponseDto<BookmarkResponseDto>>> {
     const result = await this.bookmarkService.getPaginatedBookmark(user, dto);
     return CommonResponseDto.success(ResponseMessage.READ_SUCCESS, result);
   }
 
   @Post()
   @UseGuards(TokenAuthGuard)
-  async createBookmark(@AuthUserData() user: User, @Body('cafeId') cafeId: number) {
+  async createBookmark(
+    @AuthUserData() user: User,
+    @Body('cafeId') cafeId: number,
+  ): Promise<CommonResponseDto<void>> {
     await this.bookmarkService.createBookmark(user, cafeId);
 
     return CommonResponseDto.successNoContent(ResponseMessage.CREATE_SUCCESS);
