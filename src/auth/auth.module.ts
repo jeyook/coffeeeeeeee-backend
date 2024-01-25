3834dd9d16274e10a2526b2f9abb38016c448d19
JeYeongR <wpdud2003@gmail.com>
2024-01-26 01:50:18 +0900

    feat: config 따로 빼기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,33 +2,25 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { User } from 'src/entity/user.entity';
-import { UserRole } from 'src/entity/user-role.entity';
 import { Provider } from 'src/entity/provider.entity';
+import { UserRole } from 'src/entity/user-role.entity';
+import { User } from 'src/entity/user.entity';
 
 import { UserModule } from 'src/user/user.module';
-import { AuthService } from './auth.service';
+import { jwtConfig } from '../config/jwt.config';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { GoogleOAuthStrategy } from './google-oauth.strategy';
-import { TokenAuthStrategy } from './token-auth.strategy';
 import { KakaoOAuthStrategy } from './kakao-oauth.strategy';
+import { TokenAuthStrategy } from './token-auth.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, UserRole, Provider]),
     PassportModule,
     UserModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        global: true,
-        secret: configService.get('JWT_SECRET_KEY'),
-        signOptions: { expiresIn: '7d' },
-      }),
-      inject: [ConfigService],
-    }),
+    JwtModule.registerAsync(jwtConfig),
   ],
   providers: [AuthService, GoogleOAuthStrategy, TokenAuthStrategy, KakaoOAuthStrategy],
   controllers: [AuthController],
