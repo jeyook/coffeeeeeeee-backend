61e807c144643c65440256290467860347aba0df
joonho <minhora1@gmail.com>
2024-01-27 01:24:19 +0900

    feat: section에 포함된 카페데이터추가

--- src/section/dto/section-response.dto.ts
+++ src/section/dto/section-response.dto.ts
@@ -1,13 +1,35 @@
+import { Cafe } from '../../entity/cafe.entity';
 import { Section } from '../../entity/section.entity';
 
+interface SectionCafeDto {
+  id: number;
+  placeId: number;
+  address: string;
+  name: string;
+  mapX: number;
+  mapY: number;
+}
 export class SectionResponseDto {
   private id: number;
   private name: string;
   private geom: object;
+  private cafe: SectionCafeDto[];
 
   constructor(section: Section) {
     this.id = section.id;
     this.name = section.name;
     this.geom = section.geom;
+    this.cafe = this.mapCafeToDto(section.cafe);
+  }
+
+  private mapCafeToDto(cafe: Cafe[]): SectionCafeDto[] {
+    return cafe.map((c) => ({
+      id: c.id,
+      placeId: c.placeId,
+      address: c.address,
+      name: c.name,
+      mapX: c.mapX,
+      mapY: c.mapY,
+    }));
   }
 }
