994b84e80282f3cf68ec21fc7ae89394843efd98
joonho <minhora1@gmail.com>
2024-01-31 01:26:40 +0900

    feat: sectionController 테스트코드작성

--- src/section/section.controller.spec.ts
+++ src/section/section.controller.spec.ts
@@ -1,18 +1,70 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SectionController } from './section.controller';
+import { SectionService } from './section.service';
 
 describe('SectionController', () => {
-  let controller: SectionController;
+  let sectionController: SectionController;
+
+  const mockSectionService = {
+    getSectionByName: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SectionController],
+      providers: [
+        ConfigService,
+        {
+          provide: SectionService,
+          useValue: mockSectionService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<SectionController>(SectionController);
+    sectionController = module.get<SectionController>(SectionController);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(sectionController).toBeDefined();
+  });
+
+  describe('getSectionByName()', () => {
+    const mockSectionName = 'hongdae';
+    const mockSectionResponseDto = {
+      id: 1,
+      name: 'hongdae',
+      geom: '',
+      cafe: [
+        {
+          id: 1,
+          placeId: 1,
+          address: '홍대',
+          mapX: 1,
+          mapY: 1,
+        },
+      ],
+    };
+
+    it('SUCCESS: section을 정상적으로 조회한다.', async () => {
+      const spyGetSectionByNameFn = jest.spyOn(mockSectionService, 'getSectionByName');
+      spyGetSectionByNameFn.mockResolvedValueOnce(mockSectionResponseDto);
+
+      const expectedResult = {
+        message: 'READ_SUCCESS',
+        statusCode: 200,
+        data: mockSectionResponseDto,
+      };
+
+      const result = await sectionController.getSectionByName(mockSectionName);
+
+      expect(result).toEqual(expectedResult);
+      expect(spyGetSectionByNameFn).toHaveBeenCalledTimes(1);
+      expect(spyGetSectionByNameFn).toHaveBeenCalledWith(mockSectionName);
+    });
   });
 });
