db691979be7d679fc3191a89ed888178351be824
joonho <minhora1@gmail.com>
2024-02-01 00:03:07 +0900

    feat: sectionService 테스트코드작성

--- src/section/section.service.spec.ts
+++ src/section/section.service.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { SectionService } from './section.service';
+import { Section } from '../entity/section.entity';
 
 describe('SectionService', () => {
-  let service: SectionService;
+  let sectionService: SectionService;
+
+  const mockSectionRepository = {
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SectionService],
+      providers: [
+        SectionService,
+        {
+          provide: getRepositoryToken(Section),
+          useValue: mockSectionRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<SectionService>(SectionService);
+    sectionService = module.get<SectionService>(SectionService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sectionService).toBeDefined();
+  });
+
+  describe('getSectionByName()', () => {
+    const mockSectionName = 'test';
+
+    const mockSection = {
+      createdAt: 2024,
+      updatedAt: 2024,
+      deletedAt: null,
+      id: 1,
+      name: 'hongdae',
+      geom: { type: 'Polygon', coordinates: [[Array]] },
+      cafes: [
+        {
+          createdAt: 2024,
+          updatedAt: 2024,
+          deletedAt: null,
+          id: 1,
+          placeId: 1,
+          address: '홍대1번출구',
+          name: '스타벅스',
+          mapX: 11,
+          mapY: 12,
+          phoneNumber: '010000000',
+          imageUrl: 'qewr',
+          homepageUrl: 'http://www.starbucks.co.kr',
+        },
+        {
+          createdAt: 2024,
+          updatedAt: 2024,
+          deletedAt: null,
+          id: 2,
+          placeId: 1,
+          address: '홍대2번출구',
+          name: '커피빈',
+          mapX: 11,
+          mapY: 12,
+          phoneNumber: '010000000',
+          imageUrl: 'qewr',
+          homepageUrl: 'http://www.starbucks.co.kr',
+        },
+      ],
+    };
+
+    it('SUCCESS: section을 정상적으로 조회한다.', async () => {
+      const spySectionFindOneFn = jest.spyOn(mockSectionRepository, 'findOne');
+      spySectionFindOneFn.mockResolvedValueOnce(mockSection);
+
+      const expectedResult = mockSection;
+
+      const result = await sectionService.getSectionByName(mockSectionName);
+
+      expect(result).toEqual(expectedResult);
+      expect(spySectionFindOneFn).toHaveBeenCalledTimes(1);
+      expect(spySectionFindOneFn).toHaveBeenCalledWith({ name: mockSectionName });
+    });
   });
 });
