489335b83d583418956f30b4b9f6f47261caee78
joonho <minhora1@gmail.com>
2024-02-02 23:13:18 +0900

    feat: swagger 생성

--- src/section/section.controller.ts
+++ src/section/section.controller.ts
@@ -3,12 +3,20 @@ import { SectionService } from './section.service';
 import { CommonResponseDto } from '../common/dto/common-response.dto';
 import { ResponseMessage } from '../common/dto/response-message.enum';
 import { SectionResponseDto } from './dto/section-response.dto';
-
+import {
+  ForbiddenResponse,
+  GetSectionByNameOperation,
+  GetSectionByNameResponse,
+} from './decorator/section-swagger-decorator';
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('section')
 @Controller('section')
 export class SectionController {
   constructor(private readonly sectionsService: SectionService) {}
-
   @Get('/')
+  @GetSectionByNameResponse
+  @GetSectionByNameOperation
+  @ForbiddenResponse
   async getSectionByName(
     @Query('name') sectionName: string,
   ): Promise<CommonResponseDto<SectionResponseDto>> {
